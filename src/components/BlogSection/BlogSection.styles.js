3f35fb62da35815e506a386e61a0b1e953eeb845
Alex <alexwaller301@gmail.com>
2021-10-31 08:55:08 -0400

    update

--- src/components/BlogSection/BlogSection.styles.js
+++ src/components/BlogSection/BlogSection.styles.js
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    width: 100%;
+    height: 876px;
+    padding: 120px 0 200px;
+    background-color: #191919;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+    width: 1120px;
+    height: 480px;
+    margin: 0 351.5px;
+    padding: 0 40px;
+    display: flex;
+    overflow: hidden;
+`;
+
+export const BlogPost = styled.div`
+    width: 1120px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
--- src/components/BlogSection/index.js
+++ src/components/BlogSection/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 //Styles
-import {Wrapper, Content} from "./BlogSection.styles";
+import {Wrapper, Content, BlogPost} from "./BlogSection.styles";
 
 const BlogSection = () => {
     return (
         <Wrapper>
             <Content>
-                Blog section
+                <BlogPost>
+                <img src="https://www.nuraphone.com/cdn-global/5b2c8a9fa49e8e2d4604e60b/5f59b0b830faef5f37baa9e8_blog-movie-p-500.png" alt="blog"/>
+                </BlogPost>
+
+                <BlogPost>
+                <img src="https://www.nuraphone.com/cdn-global/5b2c8a9fa49e8e2d4604e60b/5f59b0b830faef5f37baa9e8_blog-movie-p-500.png" alt="blog"/>
+                </BlogPost>
+
+                <BlogPost>
+                <img src="https://www.nuraphone.com/cdn-global/5b2c8a9fa49e8e2d4604e60b/5f59b0b830faef5f37baa9e8_blog-movie-p-500.png" alt="blog"/>
+                </BlogPost>
             </Content>
         </Wrapper>
     )
