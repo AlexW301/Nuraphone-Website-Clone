5c6bf67a492f1bc0e845d6cdc96d92effa65f926
Alex <alexwaller301@gmail.com>
2021-10-31 14:19:21 -0400

    update

--- src/components/BlogSection/BlogSection.styles.js
+++ src/components/BlogSection/BlogSection.styles.js
@@ -5,31 +5,93 @@ export const Wrapper = styled.div`
     height: 876px;
     padding: 120px 0 200px;
     background-color: #191919;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .nurablog {
+        color: #074dff;
+        font-size: 3rem;
+        width: 1420px;
+        padding: 0 225px;
+    }
+
+    div ul {
+        display: flex;
+    }
+
+    div ul li button {
+        color: #fff;
+        font-size: 3rem;
+    }
 `;
 
 export const Content = styled.div`
-    width: 1120px;
+    width: 1420px;
     height: 480px;
     margin: 0 351.5px;
     padding: 0 40px;
     display: flex;
-    overflow: hidden;
+    gap: 300px;
+    position: relative;
+   
+    transition: transform ease-out .5s;
+    transform: translateX(-2240px);
+    
 `;
 
 export const BlogPost = styled.div`
-    min-width: 1120px;
+    min-width: 1420px;
+    max-width: 1420px;
     height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
+    gap: 160px;
     
 
     img {
         width: 500px;
     }
 
+    .pic-one {
+        height: 480px;
+        width: 480px;
+        object-fit: cover;
+    }
+
     .slide {
         transition: transform ease-out .5s;
-        transform: translateX(-1120px);
     }
+
+    .post-content {
+        width: 405px;
+        height: 459.19px;
+    }
+
+    .post-heading {
+        font-size: 4.1rem;
+        line-height: 56px;
+        font-weight: 500;
+        color: #fff;
+        margin: 20px 0 30px;
+    }
+
+    .post-para {
+        font-size: 2.4rem;
+        line-height: 36px;
+        margin: 0 0 60px;
+        color: #fff;
+    }
+
+    .link:link,
+    .link:visited { 
+        font-size: 1.8rem;
+        text-decoration: none;
+        border-bottom: 1px solid #fff;
+        color: #fff;
+    }
+
 `;
--- src/components/BlogSection/index.js
+++ src/components/BlogSection/index.js
@@ -1,23 +1,45 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, {useRef} from "react";
 //Styles
 import {Wrapper, Content, BlogPost} from "./BlogSection.styles";
 
 const BlogSection = () => {
+    const slideShow = useRef(null);
+
+    const changeSlide = (slide) => {
+        slideShow.current.style.transform = `translateX(${slide}px)`;
+    }
+
     return (
         <Wrapper>
-            <Content>
-                <BlogPost>
-                <img src="https://www.nuraphone.com/cdn-global/5b2c8a9fa49e8e2d4604e60b/5f59b0b830faef5f37baa9e8_blog-movie-p-500.png" alt="blog"/>
+            <p className="nurablog">Nura Blog</p>
+            <Content ref={slideShow}>
+                <BlogPost className="slide">
+                <div className="post-content">
+                    <h2 className="post-heading">Best Movie Soundtracks for Your Nuraphone</h2>
+                    <p className="post-para">From Joker (2019) to Iron Man (2008) these film scores will sound amazing on your Nuraphone.</p>
+                    <a className="link" href="#">Find Out More</a>
+                </div>
+                <img className="pic-one" src="https://www.nuraphone.com/cdn-global/5b2c8a9fa49e8e2d4604e60b/5f59b0b830faef5f37baa9e8_blog-movie-p-500.png" alt="blog"/>
                 </BlogPost>
 
-                <BlogPost className="slide">
+                <BlogPost  className="slide">
                 <img src="https://www.nuraphone.com/cdn-global/5b2c8a9fa49e8e2d4604e60b/5f59b43f3c234b554925edf6_blog-cool.png" alt="blog"/>
                 </BlogPost>
 
-                <BlogPost>
-                <img src="https://www.nuraphone.com/cdn-global/5b2c8a9fa49e8e2d4604e60b/5f59b0b830faef5f37baa9e8_blog-movie-p-500.png" alt="blog"/>
+                <BlogPost className="slide">
+                <img src="https://www.nuraphone.com/cdn-global/5b2c8a9fa49e8e2d4604e60b/5f59b5259ccb436c9a62acdd_blog-anc-p-500.png" alt="blog"/>
                 </BlogPost>
+
+                
             </Content>
+                <div>
+                    <ul>
+                        <li><button onClick={() => {changeSlide(0)}}>.</button></li>
+                        <li><button onClick={() => {changeSlide(-1720)}}>.</button></li>
+                        <li><button onClick={() => {changeSlide(-3440)}}>.</button></li>
+                    </ul>
+                </div>
         </Wrapper>
     )
 }
