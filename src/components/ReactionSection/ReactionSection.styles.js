2f689e8149f75469460a7d497c63464cc809cf7f
Alex <alexwaller301@gmail.com>
2021-11-06 15:05:41 -0400

    Update ReactionSection.styles.js

--- src/components/ReactionSection/ReactionSection.styles.js
+++ src/components/ReactionSection/ReactionSection.styles.js
@@ -4,6 +4,14 @@ export const Wrapper = styled.div`
     height: 787.19px;
     width: 100%;
     padding: 80px 0;
+
+    @media screen and (max-width: 1124px) {
+        height: 500px;
+    }
+
+    @media screen and (max-width: 646px) {
+        height: 400px;
+    }
 `;
 
 export const Content = styled.div`
